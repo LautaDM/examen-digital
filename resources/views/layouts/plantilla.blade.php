852dd46d16ca4834b41d3d954412ea09d90d2d56
LautaDM <53785414+LautaDM@users.noreply.github.com>
2020-02-29 21:14:23 -0300

    Add files via upload

--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -5,28 +5,70 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/estilo.css">
     <title>Nombre generico</title>
 </head>
 <body>
     <main class="container">
     <header>
-        <nav>
-            <ul>
-                <li><a href="/actores">Actores</a></li>
-                <li><a href="/agregaractor">Agregar</a></li>
-                <li><a href="/peliculas">Peliculas</a></li>
-                <li>Item1</li>
-                <li>Item1</li>
-                <li>Item1</li>
-            </ul>
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
+            <div class="container">
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto dropdown">
+                        <li class="nav-item"><a href="/peliculas" class="nav-link">Peliculas</a></li>
+                        <li class="nav-item"><a href="/actores" class="nav-link">Actores</a></li>
+                        <li class="nav-item"><a href="/agregaractor" class="nav-link">Agregar Actor</a></li>
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        
+                        <!-- Authentication Links -->
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">Iniciar sesion</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">Registrarse</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
         </nav>
 
-    </header>
+    </header><br>
 
     @yield('contenido')
 
-    <footer>  Soy un super footer   </footer>
+    <footer class="card-footer bg-dark footersito">  Soy un super footer   </footer>
 
     </main>
     <!-- Scrips de bootstrap !-->
