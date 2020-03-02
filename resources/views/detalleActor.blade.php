290db446de49f968b1eda4bfc4a8819327e1cfae
LautaDM <53785414+LautaDM@users.noreply.github.com>
2020-03-02 15:10:56 -0300

    Add files via upload

--- resources/views/detalleActor.blade.php
+++ resources/views/detalleActor.blade.php
@@ -5,11 +5,16 @@
     <center>
     <h1> {{$actorElegido->first_name . ' ' . $actorElegido->last_name}} </h1>
 
-    <li>Raiting: {{$actorElegido->rating}}</li>
-    <li>Pelicula favorita: @if ($actorElegido->movie)
+    <li class="list-unstyled">Raiting: {{$actorElegido->rating}}</li>
+    <li class="list-unstyled">Pelicula favorita: @if ($actorElegido->movie)
          {{$actorElegido->movie->title}}
          @else No tiene pelicula favorita
-        @endif </li>
+        @endif </li><br><br>
+    <h2>Peliculas en las cuales actuo</h2><br>
+    @foreach ($actorElegido->movies as $pelicula)
+    
+    <li class="list-unstyled"> {{$pelicula->title}} </li>
+    @endforeach
     </center>
     
 @endsection
--- resources/views/detallePelicula.blade.php
+++ resources/views/detallePelicula.blade.php
@@ -1,21 +1,9 @@
 @extends('layouts.plantilla')
-    
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="css/estilo.css">
-    <title> {{$peliculaElegida->title}} </title>
-</head>
-<body>
+
     
 
 @section('contenido')
-
+<div class="col-xs-12 col-md-6 col-lg-6 list-unstyled ">
     <h1> {{$peliculaElegida->title}} </h1>
     <li>Raiting:  {{$peliculaElegida->rating}}  </li>
     <li>Premios:  {{$peliculaElegida->awards}}  </li>
@@ -25,16 +13,18 @@
         {{$peliculaElegida->genres->name}}
         @else No tiene genero
     @endif</li>
-    
+</div>
+<div class="col-xs-12 col-md-6 col-lg-6 list-unstyled">
     <h2>Actores</h2>
+
         @foreach ($peliculaElegida->actors as $actor)
             <li> {{$actor->first_name . " " . $actor->last_name}} </li>
         @endforeach
-
+</div>
     <br><br><br><br>
     <center>
-    <div class="formulario">
-        <h2>Editar pelicula</h2>
+    <div class="col-12">
+        <h2>Editar pelicula</h2>    
         <form action="/pelicula/{{$peliculaElegida->id}}" method="post">
             @csrf
             <input type="text" placeholder="Titulo" name="title"><br><br>
@@ -43,18 +33,16 @@
             <input type="number" placeholder="Duracion" name="length"><br><br>
             <label for="release_date">Fecha estreno</label>
             <input type="date" name="release_date"><br><br>
+            <label for="genre_id">Genero  </label>
             <select name="genre_id">
                 @foreach ($generos as $genero)
                     <option value=" {{$genero->id}} "> {{$genero->name}} </option>
                 @endforeach
             </select><br><br>
-            <input type="submit">
+            <input type="submit" class="btn bg-info" value="Editar"><br><br>
 
         </form>
     </div>
     </center>
 
 @endsection
-
-</body>
-</html>
